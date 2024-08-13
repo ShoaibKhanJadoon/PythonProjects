69eb344a782f7b9888931abd79e83627e513e055
ShoaibKhanJadoon <140436897+ShoaibKhanJadoon@users.noreply.github.com>
2024-08-13 16:27:41 +0000

    getter and setter.py is modified

---
+++ getter and setter.py
@@ -0,0 +1,26 @@
+class myClass:
+
+  def __init__(self, value):
+    self.value = value
+    print(self.value)
+
+  def show(self):
+    print(self.value)
+
+  @property  #getter
+  def getter(self):
+    return self.value
+
+  @getter.setter
+  def setter(self, value):
+    self.value = value
+
+
+obj = myClass(10)
+#obj.show()
+print(obj.getter)
+obj.setter = 20
+obj.show()
+print(obj.getter)
+obj.setter = 30
+obj.show()
